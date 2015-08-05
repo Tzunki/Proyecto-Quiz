125e954c9e2b3cffed5591b1dfa51d0c2f3045d6
Tzunki <xlinaresd@gmail.com>
2015-08-05 23:29:32 +0200

    Crear comentarios

--- models/models.js
+++ models/models.js
@@ -29,7 +29,14 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 //Importar la definición de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
+//Importar la definición de la tabla Comment
+var Comment = sequelize.import(path.join(__dirname, 'comment'));
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //exportar definición de la tabla Quiz
+exports.Comment = Comment;
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en DB
 sequelize.sync().success(function() {
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,14 +13,16 @@ router.get('/', function(req, res) {
 router.param('quizId', quizController.load); //Autoload :quizId
 
 // Rutas de /quizzes
-router.get('/quizzes',                      quizController.index);
-router.get('/quizzes/:quizId(\\d+)',        quizController.show);
-router.get('/quizzes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizzes/new',                  quizController.new);
-router.post('/quizzes/create',              quizController.create);
-router.get('/quizzes/:quizId(\\d+)/edit',   quizController.edit);
-router.put('/quizzes/:quizId(\\d+)',        quizController.update);
-router.delete('/quizzes/:quizId(\\d+)',     quizController.destroy);
+router.get('/quizzes',                            quizController.index);
+router.get('/quizzes/:quizId(\\d+)',              quizController.show);
+router.get('/quizzes/:quizId(\\d+)/answer',       quizController.answer);
+router.get('/quizzes/new',                        quizController.new);
+router.post('/quizzes/create',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',         quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',              quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',           quizController.destroy);
+router.get('/quizzes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizzes/:quizId(\\d+)/comments',    commentController.create);
 
 router.get('/author', function(req, res) {
   res.render('author', { errors: []});
